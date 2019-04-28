01715513ac553569d23cd23f5dd4f7f95549b090
Annette Odingo <odingoannette@gmail.com>
2019-04-28 17:36:27 +0300

    Declare triangleTracker Function and the triangle's sides and output

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function triangleTracker() {
+  var value1 = parseInt(document.getElementById("a").value);
+  var value2 = parseInt(document.getElementById("b").value);
+  var value3 = parseInt(document.getElementById("c").value);
+  var output = document.getElementById("output");
