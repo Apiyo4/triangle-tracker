148fba8a292643f2da8e4d53d840b6849fe3f562
Annette Odingo <odingoannette@gmail.com>
2019-04-29 09:38:46 +0300

    Space and ensure that triangle tracker works

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,12 @@ function triangleTracker() {
   if (((value1 + value2 > value3) && (value1 + value3 > value2) && (value2 + value3 > value1)) && (value1 !== 0 || value2 !== 0 || value3 !== 0)) {
     if (value1 === value2 && value2 === value3) {
       output.textContent = "I'm an equilateral triangle";
-    } else if ((value1!== value2 && value1!== value3) && (value2!== value1 && value2 !== value3)) {
+    } else if ((value1!== value2 && value1!== value3) && (value2!== value1 && value2!== value3)) {
       output.textContent = "I'm a scalene triangle";
-    } else if ((value1 === value2 && value1 !== value3) || or(value1 === value3 && value1 !== value2) || (value2 === value3 && value2 !== value3)) {
+    } else if ((value1 === value2 && value1!== value3) || or(value1 === value3 && value1!== value2) || (value2 === value3 && value2!== value3)) {
       output.textContent = "I'm an isosceles triangle";
     }
   } else {
     output.textContent = "I'm not a triangle";
   }
-}
 }
